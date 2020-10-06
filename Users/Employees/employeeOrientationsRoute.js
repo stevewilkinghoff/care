01b54528e1bbcddd377f67ca0492c37157d8b1bd
stevewilkinghof <22236093+stevewilkinghoff@users.noreply.github.com>
2020-10-06 12:30:01 -0600

    change name

--- Users/Employees/employeeOrientationsRoute.js
+++ Users/Employees/employeeOrientationsRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 //const { check } = require('express-validator');
 
-const EmployeeOrientationController = require('./employeeOrientationController');
+const EmployeeOrientationController = require('./employeeorientationcontroller');
 
 const router = express.Router();
 
