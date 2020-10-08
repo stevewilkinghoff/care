be23ecbf45f542a4b132573840cfaf968c61117a
stevewilkinghof <22236093+stevewilkinghoff@users.noreply.github.com>
2020-10-08 16:29:10 -0600

    new build

---
+++ client/build/precache-manifest.242969b11fc3fc8065851eeb2108fa61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdf8f0594768cfd2ed2c54fc2204ca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1438a1de1d7592b47946",
+    "url": "/static/css/2.1b9e66f6.chunk.css"
+  },
+  {
+    "revision": "912509544dedb4be4c55",
+    "url": "/static/css/main.6e5013c4.chunk.css"
+  },
+  {
+    "revision": "1438a1de1d7592b47946",
+    "url": "/static/js/2.db216a5c.chunk.js"
+  },
+  {
+    "revision": "3c9914bdb07d995b3a6e33df8b9ec8dd",
+    "url": "/static/js/2.db216a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "912509544dedb4be4c55",
+    "url": "/static/js/main.4f010d91.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
