4c712cf83b97c4058aaadcab4f5a74da10a89238
stevewilkinghof <22236093+stevewilkinghoff@users.noreply.github.com>
2020-10-09 04:04:46 -0600

    changes for deploy 1

--- index.js
+++ index.js
@@ -78,7 +78,8 @@ app.get('/', (req, res) => {
 
 if (process.env.NODE_ENV==='production'){
     // Express will serve up production assets
-    app.use(express.static('client/build'));
+    //app.use(express.static('client/build')); (from Maximillian)
+    app.use(express.static(path.join(__dirname, 'build')));
 
     // Express will serve up html file if it doesnt' recognize route
     const path = require('path');
